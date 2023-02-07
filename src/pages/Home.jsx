44078d4a461999ccdee98dc9d67cd9d72898b965
pakxe <pigkill40@naver.com>
2023-02-08 04:05:47 +0900

    chore: render prop 연습

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import Toggle from './Toggle';
 
+const Type = ({ on }) => <div>{on ? '켜진' : '꺼진'}상태인 버튼이야.</div>;
 // 사용하는 곳에서 state를 관리할 필요가 없어졌다!
 const Home = () => (
   <Toggle>
     <Toggle.Title />
-    <Toggle.Button>click me</Toggle.Button>
+    <Toggle.Button renderProp={(on) => <Type on={on} />}>click me</Toggle.Button>
   </Toggle>
 );
 
--- src/pages/ToggleButton.jsx
+++ src/pages/ToggleButton.jsx
@@ -6,11 +6,12 @@ export const ToggleTitle = () => {
   return on ? <div>켜졌습니다.</div> : <div>꺼졌습니다.</div>;
 };
 
-export const ToggleButton = ({ children }) => {
-  const { toggle } = useContext(ToggleContext);
+export const ToggleButton = ({ children, renderProp }) => {
+  const { on, toggle } = useContext(ToggleContext);
 
   return (
-    <button type='button' onClick={toggle}>
+    <button type='button' onClick={toggle} renderProp={renderProp}>
+      {renderProp(on)}
       {children}
     </button>
   );
