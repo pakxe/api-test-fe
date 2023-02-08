b4d3069232e6223b1d9b1dfa2404f20a53249407
pakxe <pigkill40@naver.com>
2023-02-08 23:55:53 +0900

    feat: three.js 를 이용한 3d 차량 모델 띄우는 연습

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,35 @@
-import Toggle from './Toggle';
+import { OrbitControls, PerspectiveCamera, Stars } from '@react-three/drei';
+import { Canvas, Camera, useLoader } from '@react-three/fiber';
+import { useRef } from 'react';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-const Type = ({ on }) => <div>{on ? '켜진' : '꺼진'}상태인 버튼이야.</div>;
-// 사용하는 곳에서 state를 관리할 필요가 없어졌다!
-const Home = () => (
-  <Toggle>
-    <Toggle.Title />
-    <Toggle.Button renderProp={(on) => <Type on={on} />}>click me</Toggle.Button>
-  </Toggle>
-);
+const Model = () => {
+  const gltf = useLoader(GLTFLoader, 'old_mg_car/scene.gltf'); // pwd : public 폴더. 얘를 기준으로 위치 기입
+  // const gltf = useLoader(GLTFLoader, '../src/pages/old_mg_car/scene.gltf');
+  return (
+    <>
+      <primitive object={gltf.scene} scale={0.4} />
+    </>
+  );
+};
+
+function Home() {
+  return (
+    <div style={{ height: '500px' }}>
+      <Canvas height='100%' style={{ backgroundColor: 'black' }}>
+        <ambientLight />
+        <OrbitControls />
+        {/* <pointLight position={[10, 10, 10]} /> */}
+        {/* <ambientLight intensity={0.5} /> */}
+        <spotLight position={[10, 15, 10]} angle={0.3} />
+        <mesh position={[0, 0, 0]}>
+          <Stars />
+          <meshStandardMaterial color={0x00ff00} />
+          <Model />
+        </mesh>
+      </Canvas>
+    </div>
+  );
+}
 
 export default Home;
